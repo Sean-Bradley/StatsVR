4ff5b37c9c3a51dc56cf1d2800ead6bdb1211466
seanwasere youtube <seanwasere@gmail.com>
2018-03-24 19:17:40 +0000

    Add files via upload

--- statsvr.js
+++ statsvr.js
@@ -18,7 +18,7 @@
         var canvas = document.createElement('canvas');
         canvas.width = 64;
         canvas.height = 64;
-        var ctx = canvas.getContext('2d');       
+        var ctx = canvas.getContext('2d');
 
         var texture = new THREE.Texture(canvas);
         var material = new THREE.MeshBasicMaterial({ map: texture, depthTest: false, transparent: true });
@@ -33,11 +33,18 @@
         pivot.add(statsPlane);
         scene.add(pivot);
 
-        var startTime = Date.now();
-        var lastTime = startTime;
-        var frames = 0;
+        var timer = (performance || Date);
+        var statsDisplayRefreshDelay  = 1000;
 
-        var graphData = new Array(32).fill(0);
+        var fpsLastTime = timer.now();
+        var fpsFrames = 0;
+        var fpsGraphData = new Array(32).fill(0);
+
+        var msActive = false;
+        var msStart = timer.now();
+        var msEnd = timer.now();
+        var msGraphData = new Array(32).fill(0);
+        var ms = 0;
 
         var custom1 = null;
         var custom2 = null;
@@ -63,51 +70,82 @@
                 custom3 = val;
             },
 
+            msStart: function (val) {
+                msActive = true;
+                msStart = timer.now();
+            },
+            msEnd: function (val) {
+                msEnd = timer.now();
+                //console.log(msStart + " " + msEnd);
+                ms = (((msEnd - msStart) * 100) / 100).toFixed(2);
+            },
+
             update: function () {
                 pivot.rotation.copy(camera.rotation);
-                pivot.position.setFromMatrixPosition( camera.matrixWorld );
+                pivot.position.setFromMatrixPosition(camera.matrixWorld);
 
                 texture.needsUpdate = true;
 
-                var now = Date.now();
-                var dt = now - lastTime;
-                frames++;
-                if (now > lastTime + 1000) {
-                    lastTime = now;
-                    var FPS = Math.round(((frames * 1000) / dt) * 100) / 100;
-                    frames = 0;
-                                        
-                    graphData.push(parseInt(FPS));
-                    if (graphData.length >= 32) {
-                        graphData.shift();
-                    }
-                    var ratio = Math.max.apply(null, graphData);
-                    
+                var now = timer.now();
+                var dt = now - fpsLastTime;
+                fpsFrames++;
+                if (now > fpsLastTime + statsDisplayRefreshMS) {
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                    //FPS
+                    fpsLastTime = now;
+                    var FPS = ((((fpsFrames * 1000) / dt) * 100) / 100).toFixed(2);
+                    fpsFrames = 0;
+
+                    fpsGraphData.push(FPS);
+                    if (fpsGraphData.length >= 32) {
+                        fpsGraphData.shift();
+                    }
+                    var ratio = Math.max.apply(null, fpsGraphData);
 
                     ctx.strokeStyle = '#035363';
                     for (var i = 0; i < 32; i++) {
                         ctx.beginPath();
                         ctx.moveTo(i, 16);
-                        ctx.lineTo(i, (16 - (graphData[i] / ratio) * 16));
+                        ctx.lineTo(i, (16 - (fpsGraphData[i] / ratio) * 16));
                         ctx.stroke();
                     }
 
                     ctx.font = "13px Calibri";
                     ctx.fillStyle = "#00cc00";
-                    ctx.fillText(FPS, 2, 13);
+                    ctx.fillText(FPS, 1, 13);
 
-                    if(custom1){
+                    //MS
+                    if (msActive) {
+                        msGraphData.push(ms);
+                        if (msGraphData.length >= 32) {
+                            msGraphData.shift();
+                        }
+                        ratio = Math.max.apply(null, msGraphData);
+                        ctx.strokeStyle = '#f35363';
+                        for (var i = 0; i < 32; i++) {
+                            ctx.beginPath();
+                            ctx.moveTo(i + 32, 16);
+                            ctx.lineTo(i + 32, (16 - (msGraphData[i] / ratio) * 16));
+                            ctx.stroke();
+                        }
+                        ctx.font = "13px Calibri";
+                        ctx.fillStyle = "#00ccff";
+                        ctx.fillText(ms, 33, 13);
+                    }
+
+                    //Custom
+                    if (custom1) {
                         ctx.font = "11px";
                         ctx.fillStyle = "#ffffff";
                         ctx.fillText(custom1, 0, 29);
                     }
-                    if(custom2){
+                    if (custom2) {
                         ctx.font = "11px";
                         ctx.fillStyle = "#ffffff";
                         ctx.fillText(custom2, 0, 45);
                     }
-                    if(custom3){
+                    if (custom3) {
                         ctx.font = "11px";
                         ctx.fillStyle = "#ffffff";
                         ctx.fillText(custom3, 0, 61);
