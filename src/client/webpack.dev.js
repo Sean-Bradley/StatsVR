64206156fcb59bf5b8b2264f40c2b6cb715ed8ba
Sean Bradley <seanwasere@gmail.com>
2022-01-27 22:44:20 +0000

    updates

--- src/client/webpack.dev.js
+++ src/client/webpack.dev.js
@@ -1,11 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'eval-source-map',
     devServer: {
-        contentBase: './dist/client',
+        https: true,
+        static: {
+            directory: path.join(__dirname, '../../dist/client'),
+        },
         hot: true,
     }
 });
